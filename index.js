0c31023f07943d31fd4ee5c33c9a7a7470a65ea4
ppidcard <tvxqlikei@live.cn>
2021-04-18 01:17:05 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+
+let driver = {Name: 'Sam'};
+
+const updateDriverWithKeyAndValue = function(obj, key, value){
+    return Object.assign({}, obj, { [key]: value });
+}
+
+const destructivelyUpdateDriverWithKeyAndValue = function(obj, key, value){
+    return Object.assign(obj, {[key]:value})
+}
+
+const deleteFromDriverByKey = function(obj, key){
+    let newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+const destructivelyDeleteFromDriverByKey = function(obj, key){
+ delete obj[key];
+ return obj;
+}
